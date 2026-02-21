8b09dbf448342ea373ed8fa8f59fceed5060c178
ahmedgamaleldeen12 <ahmedgamaleldeen12@gmail.com>
2026-02-21 13:07:23 +0200

    🧹chore: remove unnecessary code from AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('dynamic-box-selector-rxjs');
